1c9f187121617f66b4d4f77e1d12037095e7912d
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-20 14:30:14 -0700

    :new: Add template class

---
+++ src/template.js
@@ -0,0 +1,31 @@
+'use babel'
+
+export class Template {
+  constructor (wrappers = ['{', '}']) {
+    this.wrappers = wrappers
+  }
+  render(template, parameters = {}) {
+    return String(template).replace(`/\\${this.wrappers[0]}([\w\.]+)${this.wrappers[1]}\\/`, function(matches) {
+      const match = matches[1]
+      if (match.indexOf('.') === -1) {
+        if (parameters.hasOwnProperty(match)) {
+          return parameters[match]
+        } else return matches[0]
+      } else {
+        let subject = parameters
+        match.split('.').forEach(function(chunk) {
+          if (subject.hasOwnProperty(chunk)) {
+            return subject[chunk]
+          } else return ''
+        })
+        if (typeof subject === 'object' && subject) {
+          throw new Error(`Cannot use non-stringish value in template: ${match}`)
+        }
+        return subject.toString()
+      }
+    })
+  }
+  create(wrappers) {
+    return new Template(wrappers)
+  }
+}
